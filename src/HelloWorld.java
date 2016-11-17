211ec87acce79895be1be400657513bf16690dd5
ewanklomp <ewanklomp@hotmail.com>
2016-11-17 20:17:39 +0100

    Initilization

---
+++ src/HelloWorld.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		int test = 5;
+		Telop(test);
+	
+	}
+	public static int Telop (int test){
+		
+		test = test +1;
+		System.out.println(test);
+		return test;
+	}
+
+	@Test
+	public void testAdd1Plus1() 
+	{
+	    int x  = 1 ; 
+	    assertEquals(2, HelloWorld.Telop(x));
+	}
+}
